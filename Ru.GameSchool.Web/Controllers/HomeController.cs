ca214078190baecf7b8429de4498a6216cb95503
Dabbi <davidein@gmail.com>
2012-04-27 17:15:43 +0000

    Test commit.

--- Ru.GameSchool.Web/Controllers/HomeController.cs
+++ Ru.GameSchool.Web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Ru.GameSchool.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = " Test Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
         }
