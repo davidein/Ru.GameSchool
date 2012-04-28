45488ff4be409d6673da9def3c295d4eef19329a
unknown <ek@FRAKKO-PC.(none)>
2012-04-28 17:36:25 +0000

    Sma meira

---
+++ Ru.GameSchool.Web/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ru.GameSchool.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+
+                public ActionResult Student()
+        {
+            return View();
+        }
+
+                public ActionResult Course()
+        {
+            return View();
+        }
+
+                public ActionResult TeacherList()
+        {
+            return View();
+        }
+
+                public ActionResult StudentList()
+        {
+            return View();
+        }
+
+                public ActionResult CourseList()
+        {
+            return View();
+        }
+
+                public ActionResult CourseRegisterTeacher()
+        {
+            return View();
+        }
+
+                public ActionResult CourseRegisterStudent()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ru.GameSchool.Web/Controllers/TeacherController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ru.GameSchool.Web.Controllers
+{
+    public class TeacherController : Controller
+    {
+        //
+        // GET: /Teacher/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Level()
+        {
+            return View();
+        }
+
+        public ActionResult Message()
+        {
+            return View();
+        }
+
+        public ActionResult Messages()
+        {
+            return View();
+        }
+
+         public ActionResult CommentList()
+        {
+            return View();
+        }
+        
+        public ActionResult Project()
+        {
+            return View();
+        }
+        
+        public ActionResult Test()
+        {
+            return View();
+        }
+        
+        public ActionResult Lecture()
+        {
+            return View();
+        }
+        
+        public ActionResult ProjectReturned()
+        {
+            return View();
+        }
+        
+        public ActionResult CourseFront()
+        {
+            return View();
+        }
+    }
+}
