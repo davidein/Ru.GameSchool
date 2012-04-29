ee2d1bc7e8d81dd0ca41ef70d3d7cf4b41f34b02
Erling Gudjohnsen <info@liveshuttle.com>
2012-04-29 21:24:04 +0000

    CourseController -

---
+++ Ru.GameSchool.Web/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Ru.GameSchool.Web.Controllers
+{
+    public class CourseController : BaseController
+    {
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Level(int id, int? levelId)
+        {
+
+            return View();
+        }
+
+    }
+}
--- Ru.GameSchool.Web/Scripts/common.js
+++ Ru.GameSchool.Web/Scripts/common.js
@@ -1,3 +1,3 @@
 ﻿$(document).ready(function () {
-    $('.dropdown-toggle').dropdown()
+    $('.dropdown-toggle').dropdown();
 });
