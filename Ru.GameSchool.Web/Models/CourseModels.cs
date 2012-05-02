ea2db9e2529a3da387858b37a1ed7f71109c16bd
Erling Gudjohnsen <info@liveshuttle.com>
2012-05-02 18:35:37 +0000

    Course HttpPost and Model

---
+++ Ru.GameSchool.Web/Models/CourseModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ru.GameSchool.Web.Models
+{
+    public class CourseModel
+    {
+        
+    }
+}
