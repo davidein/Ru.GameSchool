2df1936f7128b0ac09dbed23a946824a424cb0a9
Erling Gudjohnsen <info@liveshuttle.com>
2012-05-02 16:43:49 +0000

    New Controllers

---
+++ Ru.GameSchool.Web/Controllers/ExamController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ru.GameSchool.Web.Controllers
+{
+    public class ExamController : BaseController
+    {
+        //
+        // GET: /Exam/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
