35b3df41f521f8f296b5b8afefaf7a6cd9d4d1b7
Erling Gudjohnsen <info@liveshuttle.com>
2012-05-02 22:27:24 +0000

    Course

--- Ru.GameSchool.BusinessLayer/Services/CourseService.cs
+++ Ru.GameSchool.BusinessLayer/Services/CourseService.cs
@@ -12,9 +12,17 @@ namespace Ru.GameSchool.BusinessLayer.Services
     /// </summary> 
     public class CourseService : BaseService
     {
+        /// <summary>
+        /// Gets a course instance through a parameter of this function and if it isn't null persist it to the database.
+        /// </summary>
+        /// <param name="userInfo">Instance of a course</param>
         public void AddCourse(Course course)
         {
-            throw new System.NotImplementedException();
+            if (course != null)
+            {
+                GameSchoolEntities.Courses.AddObject(course);
+                Save();
+            }
         }
 
         public void UpdateCourse(Course course)
@@ -22,18 +30,40 @@ namespace Ru.GameSchool.BusinessLayer.Services
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets all registered Courses
+        /// </summary>
+        /// <returns>A list of all courses.</returns>
         public IEnumerable<Course> GetCourses()
         {
+            var courses = from x in GameSchoolEntities.Courses
+                          select x;
+
+            return courses;
+        }
+
+        public IEnumerable<Course> GetCoursesByUserInfoId(int userInfoId)
+        {
+            //don't know how to implement
             throw new System.NotImplementedException();
         }
 
-        public void AddUserToCourse(UserInfo user, Course course)
+        public void AddUserToCourse(int userInfoId, int courseId)
         {
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets a courseGrade instance through a parameter of this function and if it isn't null persist it to the database.
+        /// </summary>
+        /// <param name="userInfo">Instance of a courseGrade</param>
         public void AddCourseGrade(CourseGrade courseGrade)
         {
+            if (courseGrade != null)
+            {
+                GameSchoolEntities.CourseGrades.AddObject(courseGrade);
+                Save();
+            }
             throw new System.NotImplementedException();
         }
 
@@ -42,9 +72,35 @@ namespace Ru.GameSchool.BusinessLayer.Services
             throw new System.NotImplementedException();
         }
 
-        public IEnumerable<CourseGrade> GetCourseGrades()
+        /// <summary>
+        /// Gets a list of all grades in a course
+        /// </summary>
+        /// <param name="courseId">Id of the course to get grades for.</param>
+        /// <returns>A list of all grades in a given course.</returns>
+        public IEnumerable<CourseGrade> GetCourseGrades(int courseId)
         {
-            throw new System.NotImplementedException();
+            var courseGrades = from x in GameSchoolEntities.CourseGrades
+                         where x.CourseId == courseId
+                         select x;
+
+
+            return courseGrades;
+        }
+
+        /// <summary>
+        /// Gets the grade of a certain user in a course
+        /// </summary>
+        /// <param name="courseId">Id of the Course to get the grade for.</param>
+        /// <param name="userInfo">Id of the User to get the grade for.</param>
+        /// <returns>CourseGrade entity of the user for the given course.</returns>
+        public CourseGrade GetCourseGradeByCourseIdAndUserInfoId(int courseId, int userInfoId)
+        {
+            var courseGrade = from x in GameSchoolEntities.CourseGrades
+                               where x.CourseId == courseId && x.UserInfoId == userInfoId  
+                               select x;
+
+
+            return courseGrade.FirstOrDefault();
         }
 
         public void GetCurrentUserLevel()
@@ -52,6 +108,11 @@ namespace Ru.GameSchool.BusinessLayer.Services
             throw new System.NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets a Course entity by CourseId
+        /// </summary>
+        /// <param name="courseId">Id of the course to get.</param>
+        /// <returns>A course entity.</returns>
         public Course GetCourse(int courseId)
         {
             var course = from x in GameSchoolEntities.Courses
