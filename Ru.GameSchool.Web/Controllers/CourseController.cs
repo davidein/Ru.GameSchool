2cd850be102245e29ae1a6ff5bf77ecb65597b13
Erling Gudjohnsen <info@liveshuttle.com>
2012-05-02 16:39:52 +0000

    Stuff Fix, some controllers, service fix

--- Ru.GameSchool.Web/Controllers/CourseController.cs
+++ Ru.GameSchool.Web/Controllers/CourseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Ru.GameSchool.Web.Controllers
@@ -16,11 +17,28 @@ namespace Ru.GameSchool.Web.Controllers
             return View();
         }
 
-        public ActionResult Level(int id, int? levelId)
+        public ActionResult Item(int id)
         {
 
             return View();
         }
 
+        
+        //TODO: Impliment Role Authentication
+        public ActionResult Create(int id)
+        {
+
+            return View();
+        }
+
+        //TODO: Create Post ActionResult
+
+
+        //TODO: Impliment Role Authentication
+        public ActionResult Edit(int id)
+        {
+
+            return View();
+        }
     }
 }
