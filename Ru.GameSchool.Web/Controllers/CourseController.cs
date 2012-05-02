3194feb0344c826cf1866ae66195f16cb8d619cc
Erling Gudjohnsen <info@liveshuttle.com>
2012-05-02 21:46:08 +0000

    Drasl

--- Ru.GameSchool.Web/Controllers/CourseController.cs
+++ Ru.GameSchool.Web/Controllers/CourseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ru.GameSchool.DataLayer;
 using Ru.GameSchool.Web.Models;
 
 namespace Ru.GameSchool.Web.Controllers
