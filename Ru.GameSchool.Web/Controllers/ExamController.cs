5394d2b73085567c7968cad9ce2aa828887fc826
Olij <olafurj@thekking.is>
2012-05-04 19:03:21 +0000

    Controllers, view, code

--- Ru.GameSchool.Web/Controllers/ExamController.cs
+++ Ru.GameSchool.Web/Controllers/ExamController.cs
@@ -12,9 +12,13 @@ namespace Ru.GameSchool.Web.Controllers
         // GET: /Exam/
 
         [Authorize(Roles = "Student")]
-        public ActionResult Get(int id)
+        public ActionResult Get(int? id)
         {
-            return View();
+            if (id.HasValue)
+            {
+                
+            }
+            return View("Exam");
         }
 
         [Authorize(Roles = "Student")]
@@ -24,15 +28,50 @@ namespace Ru.GameSchool.Web.Controllers
         }
 
         [Authorize(Roles = "Teacher")]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Teacher")]
+        [HttpPost]
         public ActionResult Create(int id)
         {
             return View();
         }
 
         [Authorize(Roles = "Teacher")]
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public ActionResult Edit(int? id, string temp)
         {
+            if (ModelState.IsValid)
+            {
+                if (id.HasValue)
+                {
+                    var exam = LevelService.GetLevelExam(id.Value);
+                    return View(exam);
+                }
+            }
             return View();
+        }
+
+        [Authorize(Roles = "Teacher")]
+        [HttpPost]
+        public ActionResult Edit(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id.HasValue)
+                {
+                    var exam = LevelService.GetLevelExam(id.Value);
+                    if (TryUpdateModel(exam))
+                    {
+                        LevelService.UpdateLevelExam(exam);
+                    }   
+                }
+            }
+            return View("Index");
         }
 
         /*
