94ea991d7af85dbaaa26a39eb5b110f8b416032b
Dabbi <davidein@gmail.com>
2012-05-04 18:31:15 +0000

    ViewModel Magic!!

---
+++ Ru.GameSchool.DataLayer/Repository/UserInfo.ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ru.GameSchool.DataLayer.Repository
+{
+    [MetadataType(typeof(UserInfoMetadata))]
+    public partial class UserInfo
+	{
+	}
+
+    public class UserInfoMetadata
+    {
+        [Required]
+        [Display(Name = "Notandanafn")]
+        public string Username;
+
+        [Required(ErrorMessage = @"Vantar")]
+        [Display(Name = "Netfang")]
+        public int Email;
+    }
+
+}
