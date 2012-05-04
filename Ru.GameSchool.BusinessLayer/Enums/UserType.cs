b1aecd504e7aadbe7fba52b1a7880a10e2e8d6f1
Eyjolfur Kristjansson <ek@FRAKKO-PC.(none)>
2012-05-04 00:23:51 +0000

    eitthvad rugl med gittid

---
+++ Ru.GameSchool.BusinessLayer/Enums/UserType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ru.GameSchool.BusinessLayer.Enums
+{
+    public enum UserType
+    {
+        Anonymous = 0,
+        Student = 1,
+        Teacher = 2,
+        Admin = 3
+    }
+
+    public class UserTypeResolver
+    {
+        public static UserType Get(string type)
+        {
+            UserType userType;
+            if (Enum.TryParse(type, out userType))
+                return userType;
+
+            return UserType.Anonymous;
+        }
+    }
+}
---
+++ Ru.GameSchool.CreateUser/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ru.GameSchool.BusinessLayer.Services;
+using Ru.GameSchool.DataLayer.Repository;
+
+namespace Ru.GameSchool.CreateUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInfo userInfo = new UserInfo();
+
+            userInfo.Username = "dabbi";
+            userInfo.Password = "dabbi";
+            userInfo.StatusId = 1;
+            userInfo.UserTypeId = 1;
+            userInfo.Fullname = "Dabbi e";
+            userInfo.Email = "davidein@gmail.com";
+            userInfo.CreateDateTime = DateTime.Now;
+            userInfo.DepartmentId = 1;
+
+            UserService userService = new UserService();
+
+            userService.CreateUser(userInfo);
+        }
+    }
+}
