0820a0bf0d1be7de6d32c23c15b9a57ee674441b
Erling Gudjohnsen <info@liveshuttle.com>
2012-05-05 15:46:43 +0000

    Admin stuff

--- Ru.GameSchool.Web/Controllers/AdminController.cs
+++ Ru.GameSchool.Web/Controllers/AdminController.cs
@@ -7,7 +7,7 @@ using Ru.GameSchool.Web.Models;
 
 namespace Ru.GameSchool.Web.Controllers
 {
-    [Authorize(Roles = "Administrator")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : BaseController
     {
         //
@@ -20,33 +20,38 @@ namespace Ru.GameSchool.Web.Controllers
             return View();
         }
 
-        public ActionResult EditUser(int id)
+        public ActionResult Users()
+        {
+            ViewBag.Users = UserService.GetUsers();
+
+            return View();
+        }
+
+        public ActionResult User(int? id)
         {
             return View();
         }
 
-        public ActionResult EditUser(UserModel model)
+        public ActionResult User(UserModel model, int? id)
         {
             return View();
         }
 
-        public ActionResult Course()
+        public ActionResult Courses()
+        {
+            ViewBag.Courses = CourseService.GetCourses();
+
+
+            return View();
+        }
+
+        public ActionResult Course(int? id)
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Course(CourseModel model)
-        {
-            return View();
-        }
-
-        public ActionResult UserList()
-        {
-            return View();
-        }
-
-        public ActionResult CourseList()
+        public ActionResult Course(CourseModel model, int? id)
         {
             return View();
         }
